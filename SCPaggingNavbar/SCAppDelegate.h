9e8c1834a47cec44e3fd030d40f672d80a975b00
singro <isingro@gmail.com>
2014-01-05 13:56:26 +0800

    Initial Commit

---
+++ SCPaggingNavbar/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  SCPaggingNavbar
+//
+//  Created by Singro on 1/5/14.
+//  Copyright (c) 2014 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SCPaggingNavbar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SCPaggingNavbar
+//
+//  Created by Singro on 1/5/14.
+//  Copyright (c) 2014 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
